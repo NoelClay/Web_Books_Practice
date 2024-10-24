594700654bba3b518cba1d25bdc8efe0bccdd030
NoelClay_gram17 <asdf1578@naver.com>
2024-10-25 07:07:47 +0900

    dfd

--- egoing_react/chapter01/egoingreact01/src/App.js
+++ egoing_react/chapter01/egoingreact01/src/App.js
@@ -60,6 +60,27 @@ function Create(props) {
     </article>
   )
 }
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  // let title, body = null;
+  return (
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        props.onUpdate(title, body);
+        // title ;
+        // body  = props.body;
+      }}>
+        <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{setTitle(event.target.value)}} /></p>
+        <p><textarea name="body" placeholder="body" value={body} onChange={event=>setBody(event.target.value)}></textarea></p>
+        <p><input type="submit" value="Update"></input></p>
+      </form>
+    </article>
+  )
+}
 
 function App() {
   const [mode, setMode] = useState('WELCOME');
@@ -72,6 +93,8 @@ function App() {
   ]);
   const [nextId, setNextId] = useState(4);
   let content = null;
+  let contextControl = null;
+
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web"></Article>
   } else if(mode === 'READ') {
@@ -83,7 +106,9 @@ function App() {
         body = topics[i].body;
       }
     }
-    content = <Article title={title} body={body}></Article>
+    content = <Article title={title} body={body}></Article>;
+    contextControl = <li><a href={"update/"+{id}} onClick={event=>{event.preventDefault(); setMode("UPDATE");}} >Update</a></li>;
+
   } else if(mode === 'CREATE') {
     content = <Create onCreate={(_title, _body)=>{
       const newTopic = {id:nextId, title:_title, body:_body};
@@ -94,6 +119,28 @@ function App() {
       setId(nextId);
       setNextId(nextId+1);
     }}></Create>
+  }else if(mode==="UPDATE"){
+    let title, body = null;
+    for(const element of topics) {
+      console.log(element.id, id);
+      if(element.id === id) {
+        title = element.title;
+        body = element.body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body};
+
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id == id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+
+    }}></Update>
   }
 
   return (
@@ -106,10 +153,15 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      <a href="/create" onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+        <li>
+          <a href="/create" onClick={event=>{
+            event.preventDefault();
+            setMode('CREATE');
+          }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
